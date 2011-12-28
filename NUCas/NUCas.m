53316ae9466353712bb8af85ddedcbf416f70d14
John Dzak <john.dzak@gmail.com>
2011-12-28 13:42:14 -0600

    Initial Commit

---
+++ NUCas/NUCas.m
@@ -0,0 +1,23 @@
+//
+//  NUCas.m
+//  NUCas
+//
+//  Created by John Dzak on 12/28/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "NUCas.h"
+
+@implementation NUCas
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
