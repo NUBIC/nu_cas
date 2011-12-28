e16066f61c87b6f8a928f88b2739d1a252bde8aa
John Dzak <john.dzak@gmail.com>
2011-12-28 16:07:11 -0600

    Add tests target.

---
+++ NUCasTests/NUCasTests.h
@@ -0,0 +1,13 @@
+//
+//  NUCasTests.h
+//  NUCasTests
+//
+//  Created by John Dzak on 12/28/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface NUCasTests : SenTestCase
+
+@end
