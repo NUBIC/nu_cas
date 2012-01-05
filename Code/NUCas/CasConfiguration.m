6ac8f7f83372ac2dbbf6d197926066b60776e521
John Dzak <john.dzak@gmail.com>
2012-01-05 16:32:34 -0600

    No longer throwing exception when configuration not found.

--- Code/NUCas/CasConfiguration.m
+++ Code/NUCas/CasConfiguration.m
@@ -29,10 +29,6 @@
 
         if (!path) {
             NSLog(@"NUCas.plist not found at %@/NUCas.plist", [NSBundle mainBundle]);
-            @throw([NSException 
-                    exceptionWithName:@"CasConfigurationNotFound" 
-                    reason:[NSString stringWithFormat:@"NUCas.plist not found at %@/NUCas.plist", [NSBundle mainBundle]] 
-                    userInfo:NULL]);
         }
         
         NSDictionary* settings = [[NSDictionary alloc] initWithContentsOfFile:path];
